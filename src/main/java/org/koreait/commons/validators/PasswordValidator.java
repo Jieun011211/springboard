22fcbe0d50747b7834773b7075c95d9e4705e23c
Jieun011211 <qhfltldkt@naver.com>
2023-08-11 01:21:03 +0900

    비밀번호 복잡성 세분화 구현

---
+++ src/main/java/org/koreait/commons/validators/PasswordValidator.java
@@ -0,0 +1,25 @@
+package org.koreait.commons.validators;
+
+public interface PasswordValidator {
+
+    /**
+     * 비밀번호 복잡성 체크 - 알파벳 체크
+     * @param password
+     * @param caseIncentive
+     *      false : 소문자 + 대문자가 반드시 포함되는 패턴
+     *      true : 대소문자 상관없이 포함되는 패턴
+     * @return
+     */
+    default boolean alphaCheck(String password, boolean caseIncentive) {
+
+        return false;
+    }
+    //숫자가 포함된 패턴
+    default boolean numberCheck(String password) {
+        return false;
+    }
+    //특수문자
+    default boolean specialCharsCheck(String password) {
+        return false;
+    }
+}
